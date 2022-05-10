e3e2581e91d97b55782d3baf03319233ba16dd5c
ian <witham.ian@gmail.com>
2022-05-10 21:16:38 +1200

    favicons

--- app/root.tsx
+++ app/root.tsx
@@ -18,9 +18,26 @@ import clsx from "clsx";
 
 export const links: LinksFunction = () => {
   return [
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      href: "/_static/apple-touch-icon.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      href: "/_static/favicon-32x32.png",
+    },
+    { rel: "manifest", href: "/_static/site.webmanifest" },
+    {
+      rel: "mask-icon",
+      href: "/_static/safari-pinned-tab.svg",
+      color: "#5bbad5",
+    },
+    { rel: "shortcut icon", href: "/_static/favicon.ico" },
     { rel: "stylesheet", href: tailwindStylesheetUrl },
     // NOTE: Architect deploys the public directory to /_static/
-    { rel: "icon", href: "/_static/favicon.ico" },
     {
       rel: "preconnect",
       href: "https://fonts.gstatic.com",
@@ -42,6 +59,11 @@ export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "At the Drive Thru",
   viewport: "width=device-width,initial-scale=1",
+  description:
+    "At The Drive (formerly known as The Egonomist); your premier source for rants, raves and arguments on topics from Politics to News to Social Trends, with a focus on New Zealand but with an eye to the world. Each week Dan and Dave will bring you new episodes. Sometimes deep and philosophical, sometimes filth and vitriol, we aim to be a voice of informed malcontent.",
+  "msapplication-TileColor": "#b91d47",
+  "msapplication-config": "/_static/browserconfig.xml",
+  "theme-color": "#ffffff",
 });
 
 function App() {
